20dac03d2af2b6d0d9df8d8a7255b1827dd3e6eb
trampimi <mira.trampitsch02@htl-salzburg.ac.at>
2021-06-04 15:45:29 +0200

    ColliderScript begonnen

---
+++ Assets/MyGame/Scripts/ColliderScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColliderScript : MonoBehaviour
+{
+    public GameObject Maus;
+    private void OnTriggerEnter(BoxCollider other)
+    {
+        Debug.Log("maus eingesammelt");
+
+        Maus.transform.position(1, 1, 1);
+        // Destroy(other.gameObject);
+        //this.gameObject.SetActive(false);
+    }
+}
