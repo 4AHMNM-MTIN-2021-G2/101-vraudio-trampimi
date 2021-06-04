9932ff8de242d9ba70e73425aa770872e2e93139
trampimi <mira.trampitsch02@htl-salzburg.ac.at>
2021-06-04 19:42:14 +0200

    maze angepasst

--- Assets/MyGame/Scripts/ColliderScript.cs
+++ Assets/MyGame/Scripts/ColliderScript.cs
@@ -5,11 +5,12 @@ using UnityEngine;
 public class ColliderScript : MonoBehaviour
 {
     public GameObject Maus;
+    private Vector3 m_Position = new Vector3(1, 1, 1);
     private void OnTriggerEnter(BoxCollider other)
     {
         Debug.Log("maus eingesammelt");
 
-        Maus.transform.position(1, 1, 1);
+        Maus.collider = tranform.Translate(1,1,1);
         // Destroy(other.gameObject);
         //this.gameObject.SetActive(false);
     }
